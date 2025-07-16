2a4dc2613610e7d819315a27041ba23d17d03808
foysalahmedmin <foysalahmedmin@gmail.com>
2025-07-17 01:43:24 +0600

    Primary Set-Up

---
+++ src/config/project/index.ts
@@ -0,0 +1,6 @@
+export const PROJECT = {
+  name: "Foysal Ahmed",
+  website: "https://foysalahmedmin.com",
+  description: "A full-stack developer",
+  version: "1.0.0",
+};
---
+++ src/config/seo/index.ts
@@ -0,0 +1,6 @@
+export const SEO = {
+  title: "Foysal Ahmed",
+  description: "A full-stack developer",
+  keywords: ["react", "ui", "library", "frontend", "backend", "full-stack", "structure"],
+  image: "/images/logo.png",
+};
