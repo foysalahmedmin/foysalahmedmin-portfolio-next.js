83b720065e7688c4d350a4307bab23eade6252ec
foysalahmedmin <foysalahmedmin@gmail.com>
2025-07-27 03:08:52 +0600

    _

--- src/app/(common)/(home)/page.tsx
+++ src/app/(common)/(home)/page.tsx
@@ -1,11 +1,11 @@
 const HomePage = () => {
   console.log("Home Page");
   
-  return <main>
-    
-    I am Foysal Ahmed (Min)
-
-  </main>;
+  return (
+    <main className="flex flex-col items-center min-h-screen justify-center">
+      <h1>I am Foysal Ahmed (Min)</h1>
+    </main>
+  );
 };
 
 export default HomePage;
